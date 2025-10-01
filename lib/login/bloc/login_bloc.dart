b956dede99bc5a78b5c81e742ee20b2c13fe4784
Bastianwx <sebastiancera2005@gmail.com>
2025-10-01 16:01:36 -0500

    nueva api

--- lib/login/bloc/login_bloc.dart
+++ lib/login/bloc/login_bloc.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 import 'login_event.dart';
 import 'login_state.dart';
 
@@ -7,14 +9,27 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     on<LoginSubmitted>((event, emit) async {
       emit(LoginLoading());
 
-      // Simulación de login
-      await Future.delayed(Duration(seconds: 2));
-
-      if (event.email == 'sebas@gmail.com' && event.password == '123456') {
-        emit(LoginSuccess(email: event.email, password: event.password));
-      } else {
-        emit(LoginError(message: 'Credenciales inválidas'));
+      try {
+        final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+        final response = await http.post(
+          url,
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({
+            'email': event.email,
+            'password': event.password,
+          }),
+        );
+        if (response.statusCode == 201) {
+         
+          emit(LoginSuccess(email: event.email, password: event.password));
+        } else {
+          emit(LoginError(message: 'Credenciales incorrectas o error del servidor.'));
+        }
+  
+      } catch (e) {
+        emit(LoginError(message: 'Error de red: ${e.toString()}'));
       }
+      
     });
   }
 }
